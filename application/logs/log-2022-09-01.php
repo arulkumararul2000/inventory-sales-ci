2a85227143ef912168cbd6742eb959fd1ef8d696
arulkumararul2000 <arulkumararul2000@gmail.com>
2022-09-16 17:33:25 +0530

    upload

---
+++ application/logs/log-2022-09-01.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-09-01 15:41:00 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-01 15:41:06 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-01 15:41:07 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-01 15:41:10 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-01 15:41:16 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-01 15:41:54 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-01 15:42:19 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-01 15:42:23 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-01 15:42:25 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-01 15:43:18 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-01 15:43:27 --> 404 Page Not Found: Public/bootstrap
---
+++ application/logs/log-2022-09-05.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-09-05 14:20:57 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-05 14:21:58 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-05 14:23:45 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-05 14:23:47 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-05 14:23:50 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-05 14:23:51 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-05 14:24:13 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-05 14:24:14 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-05 14:26:16 --> 404 Page Not Found: Public/bootstrap
+ERROR - 2022-09-05 14:26:18 --> 404 Page Not Found: Public/bootstrap
